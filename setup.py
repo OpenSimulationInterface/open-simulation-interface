a325b388e5a1a910c9426c497aa050d12b34339c
Christian Muck <cmuckmuck@gmail.com>
2017-02-08 20:05:00 +0100

    Applied PEP8 for Python files

--- setup.py
+++ setup.py
@@ -1,18 +1,20 @@
 #! python
 # -*- coding: UTF-8 -*-
 
-from setuptools import setup
-from setuptools.command.install import install
-import subprocess
 import os
+import subprocess
 import sys
 from distutils.spawn import find_executable
+
+from setuptools import setup
+from setuptools.command.install import install
 
 package_name = 'osi'
 package_path = os.path.join(os.getcwd(), package_name)
 
 
 class GenerateProtobuf(install):
+
     @staticmethod
     def find_protoc():
         """Locates protoc executable"""
@@ -35,10 +37,12 @@ class GenerateProtobuf(install):
                  'osi_sensordata.proto', 'osi_sensorspecific.proto')
 
     """ Generate Protobuf Messages """
+
     def run(self):
         for source in self.osi_files:
             sys.stdout.write('Protobuf-compiling ' + source + '\n')
-            subprocess.check_call([self.find_protoc(), '--proto_path=.', '--python_out='+package_path, './' + source])
+            subprocess.check_call([self.find_protoc(),
+                                   '--proto_path=.', '--python_out=' + package_path, './' + source])
 
         install.run(self)
 
@@ -54,16 +58,13 @@ except Exception:
 
 setup(name='open-simulation-interface',
       version='2.0.0',
-      description='A generic interface for the environmental perception of automated driving functions in virtual '
-                  'scenarios.',
-      author='Carlo van Driesten, Timo Hanke, Nils Hirsenkorn, Pilar Garcia-Ramos, Mark Schiementz, '
-             'Sebastian Schneider',
-      author_email='Carlo.van-Driesten@bmw.de, Timo.Hanke@bmw.de, Nils.Hirsenkorn@tum.de, Pilar.Garcia-Ramos@bmw.de,'
-                   'Mark.Schiementz@bmw.de, Sebastian.SB.Schneider@bmw.de',
+      description='A generic interface for the environmental perception of automated driving functions in virtual scenarios.',
+      author='Carlo van Driesten, Timo Hanke, Nils Hirsenkorn, Pilar Garcia-Ramos, Mark Schiementz, Sebastian Schneider',
+      author_email='Carlo.van-Driesten@bmw.de, Timo.Hanke@bmw.de, Nils.Hirsenkorn@tum.de, Pilar.Garcia-Ramos@bmw.de, Mark.Schiementz@bmw.de, Sebastian.SB.Schneider@bmw.de',
       packages=[package_name],
       install_requires=['protobuf'],
       cmdclass={
-        'install': GenerateProtobuf,
+          'install': GenerateProtobuf,
       },
       url='https://github.com/OpenSimulationInterface/open-simulation-interface',
       license="MPL 2.0",
@@ -71,4 +72,4 @@ setup(name='open-simulation-interface',
           'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
       ],
       data_files=[("", ["LICENSE"])]
-)
+      )
