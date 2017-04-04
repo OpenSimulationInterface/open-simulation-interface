daaf7b6588d78d7ae383ad47e8b15f28586e4d59
Philipp Roethig <Philipp.RA.Roethig@bmw.de>
2017-04-04 17:29:25 +0200

    Add two test cases for Travis to check the code style

---
+++ test_cases.py
@@ -0,0 +1,18 @@
+from glob import *
+
+
+for file in glob("*.*"):
+    if(file != "test_cases.py"):
+        with open(file, "rt") as fin:
+            i = 0
+            for line in fin:
+                i = i + 1
+
+                # Test case 1 is checking if there are illegal tabulators in the code
+                if line.find("\t") != -1:
+                    print(file + " in line " + str(i) + ": not permitted tab found")
+
+                # Test case 2 is checking if there are more than the two allowed '/'
+                if line.find("///") != -1:
+                    print(file + " in line " + str(i) + ": not permitted use of '/' ")
+
