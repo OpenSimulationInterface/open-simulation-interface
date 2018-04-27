7703c5b64a6d4c5d41ebb4a2f21f7e6ad638fe34
carsten-kuebler <32508295+carsten-kuebler@users.noreply.github.com>
2018-04-27 08:46:36 +0200

    Extend error message

--- test_cases_doc.py
+++ test_cases_doc.py
@@ -15,8 +15,9 @@ for file in glob("doc/html/*.htm*"):
             # --------------------------------------------------------------
             # Test case 1 is checking if there are illegal hash chars in the
             # documentation. -> doxygen link not found.
-            if re.search(r"([\s>]|^)#\w",line) is not None:
-                print(file + " in line " + str(i) + ": not permitted hash found")
+            matchHash = re.search(r"([\s>]|^)#\w(\S)*",line)
+            if matchHash is not None:
+                print(file + " in line " + str(i) + ": not permitted hash found. Search for: '"+line[matchHash.start():matchHash.end()])
                 state = 1
 
 
