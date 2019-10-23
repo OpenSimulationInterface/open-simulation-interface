8431055876d1811843e252b7da7d14801c37cdef
Pierre R. Mai <pmai@pmsf.de>
2019-10-23 07:29:56 +0200

    Initial addition of Traffic Update top-level msg
    Signed-off-by: Pierre R. Mai <pmai@pmsf.de>

--- setup.py
+++ setup.py
@@ -59,6 +59,7 @@ class GenerateProtobufCommand(build_py):
         'osi_hostvehicledata.proto',
         'osi_trafficsign.proto',
         'osi_trafficlight.proto',
+        'osi_trafficupdate.proto',
         'osi_roadmarking.proto',
         'osi_featuredata.proto',
         'osi_object.proto',
