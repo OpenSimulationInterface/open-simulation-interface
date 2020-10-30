541e378ddc6d2a92e7965c09282cb2a86db6f988
SergeM <sbmatyunin@gmail.com>
2020-10-30 23:57:21 +0100

    add test for OSITrace
    Signed-off-by: SergeM <sbmatyunin@gmail.com>

---
+++ tests/test_osi_trace.py
@@ -0,0 +1,21 @@
+import os
+import tempfile
+import unittest
+
+from OSITrace import OSITrace
+
+
+class TestOSITrace(unittest.TestCase):
+    def test_osi_trace(self):
+        cur_dir = os.path.dirname(os.path.abspath(__file__))
+        path = os.path.join(cur_dir, "data", "small_test.txt.lzma")
+
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            path_output = os.path.join(tmpdirname, "output.txth")
+            trace = OSITrace()
+            trace.from_file(path=path)
+            trace.make_readable(path_output, index=1)
+            trace.scenario_file.close()
+
+            self.assertTrue(os.path.exists(path_output))
+
